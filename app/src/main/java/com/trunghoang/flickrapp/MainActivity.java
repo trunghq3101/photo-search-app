e510f9e84e8d4aef48117ee0be278e70dd844072
Trung Hoàng <trunghq3101@gmail.com>
2019-01-09 11:45:47 +0700

    change style searchbox + add animation + change style main activity

--- app/src/main/java/com/trunghoang/flickrapp/MainActivity.java
+++ app/src/main/java/com/trunghoang/flickrapp/MainActivity.java
@@ -1,13 +1,8 @@
 package com.trunghoang.flickrapp;
 
-import android.app.SearchManager;
-import android.app.SearchableInfo;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.constraint.ConstraintLayout;
-import android.util.Log;
 import android.view.View;
-import android.widget.SearchView;
 
 public class MainActivity extends BaseActivity {
 
@@ -17,25 +12,8 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Log.d(TAG, "onCreate: ");
-
         setContentView(R.layout.activity_main);
 
-        activateToolbar(false);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        ConstraintLayout layout = findViewById(R.id.content_main_layout);
-        layout.requestFocus();
-        SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
-        SearchView searchView = findViewById(R.id.search_view);
-        if (searchManager != null && searchView != null) {
-            SearchableInfo searchableInfo = searchManager.getSearchableInfo(getComponentName());
-            searchView.setSearchableInfo(searchableInfo);
-            searchView.setIconifiedByDefault(false);
-        }
     }
 
     public void searchClick(View view) {
